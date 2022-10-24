3d8650c3e0d6db1b94d3814ae642a8eda313608b
rBLOT-Arcenz <r.blot@protonmail.com>
2022-10-24 16:24:29 +0200

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,37 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     protected Dictionary<Cards, int> Cards = new Dictionary<Cards, int>();
+    protected Dictionary<Etablissement, bool> Etablissement = new Dictionary<Etablissement, bool>;
     protected int Gold;
     protected string Name;
 
+    private Boulangerie boulangerie = new Boulangerie();
+    private Cafe cafe = new Cafe();
+    private Centredaffaires centredaffaires = new Centredaffaires();
+    private Centredetele centredetele = new Centredetele();
     private Champsdeble champsdeble = new Champsdeble();
+    private Fabriquedemeuble fabriquedemeuble = new Fabriquedemeuble();
+    private Ferme ferme = new Ferme();
+    private Forêt forêt = new Forêt();
+    private Fromagerie fromagerie = new Fromagerie();
+    private Marchedefruitsetlegumes marchedefruitsetlegumes = new Marchedefruitsetlegumes();
+    private Mine mine = new Mine();
+    private Restaurant restaurant = new Restaurant();
+    private Stade stade = new Stade();
+    private Superette superette = new Superette();
+    private Verger verger = new Verger();
+
+    private Centrecommercial centrecommercial = new Centrecommercial();
+    private Gare gare = new Gare();
+    private Parcdattraction parcdattraction = new Parcdattraction();
+    private Tourradio tourradio = new Tourradio();
 
     public Player(string name)
     {
         Gold = 0;
         Name = name;
-        Cards = new Dictionary<Cards, int> { {champsdeble, 1 }, { new Champsdeble(), 1 } };
+        Cards = new Dictionary<Cards, int> { {boulangerie, 1}, {cafe, 0 }, {centredaffaires, 0 }, {centredetele, 0 }, {champsdeble, 1 }, {fabriquedemeuble, 0 }, {ferme , 0 }, {forêt, 0 }, { fromagerie, 0 }, {marchedefruitsetlegumes, 0 }, {mine, 0 }, {restaurant, 0 }, {stade, 0 }, {superette, 0 }, {verger, 0 } };
+        Etablissement = new Dictionary<Etablissement, bool> { { centrecommercial, false }, { gare, false }, { parcdattraction, false }, { tourradio, false } };
         Cards[champsdeble] += 1;
     }
 
