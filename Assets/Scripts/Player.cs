b42ebc789f6798dee833f6cc69e223e3ca32664d
rBLOT-Arcenz <r.blot@protonmail.com>
2022-10-25 09:36:44 +0200

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,39 +4,44 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    protected Dictionary<Cards, int> Cards = new Dictionary<Cards, int>();
-    protected Dictionary<Etablissement, bool> Etablissement = new Dictionary<Etablissement, bool>();
+    protected Dictionary<Cards, int> cards = new Dictionary<Cards, int>();
+    protected Dictionary<Etablissement, bool> etablissement = new Dictionary<Etablissement, bool>();
     protected int Gold;
     protected string Name;
 
-    private Boulangerie boulangerie = new Boulangerie();
-    private Cafe cafe = new Cafe();
-    private Centredaffaires centredaffaires = new Centredaffaires();
-    private Chainedetele chainedetele = new Chainedetele();
-    private Champsdeble champsdeble = new Champsdeble();
-    private Fabriquedemeuble fabriquedemeuble = new Fabriquedemeuble();
-    private Ferme ferme = new Ferme();
-    private Forêt forêt = new Forêt();
-    private Fromagerie fromagerie = new Fromagerie();
-    private Marchedefruitsetlegumes marchedefruitsetlegumes = new Marchedefruitsetlegumes();
-    private Mine mine = new Mine();
-    private Restaurant restaurant = new Restaurant();
-    private Stade stade = new Stade();
-    private Superette superette = new Superette();
-    private Verger verger = new Verger();
+    public Boulangerie boulangerie = new Boulangerie();
+    public Cafe cafe = new Cafe();
+    public Centredaffaires centredaffaires = new Centredaffaires();
+    public Chainedetele chainedetele = new Chainedetele();
+    public Champsdeble champsdeble = new Champsdeble();
+    public Fabriquedemeuble fabriquedemeuble = new Fabriquedemeuble();
+    public Ferme ferme = new Ferme();
+    public Forêt forêt = new Forêt();
+    public Fromagerie fromagerie = new Fromagerie();
+    public Marchedefruitsetlegumes marchedefruitsetlegumes = new Marchedefruitsetlegumes();
+    public Mine mine = new Mine();
+    public Restaurant restaurant = new Restaurant();
+    public Stade stade = new Stade();
+    public Superette superette = new Superette();
+    public Verger verger = new Verger();
 
-    private Centrecommercial centrecommercial = new Centrecommercial();
-    private Gare gare = new Gare();
-    private Parcdattraction parcdattraction = new Parcdattraction();
-    private Tourradio tourradio = new Tourradio();
+    public Centrecommercial centrecommercial = new Centrecommercial();
+    public Gare gare = new Gare();
+    public Parcdattraction parcdattraction = new Parcdattraction();
+    public Tourradio tourradio = new Tourradio();
 
     public Player(string name)
     {
         Gold = 0;
         Name = name;
+<<<<<<< Updated upstream
         Cards = new Dictionary<Cards, int>() { { boulangerie, 1 }, { cafe, 0 }, { centredaffaires, 0 }, { chainedetele, 0 }, { champsdeble, 1 }, { fabriquedemeuble, 0 }, { ferme, 0 }, { forêt, 0 }, { fromagerie, 0 }, { marchedefruitsetlegumes, 0 }, { mine, 0 }, { restaurant, 0 }, { stade, 0 }, { superette, 0 }, { verger, 0 } };
         Etablissement = new Dictionary<Etablissement, bool>() { { centrecommercial, false }, { gare, false }, { parcdattraction, false }, { tourradio, false } };
         Cards[champsdeble] += 1;
+=======
+        cards = new Dictionary<Cards, int>(){ {boulangerie, 1}, {cafe, 0 }, {centredaffaires, 0 }, { chainedetele, 0 }, {champsdeble, 1 }, {fabriquedemeuble, 0 }, {ferme , 0 }, {forêt, 0 }, { fromagerie, 0 }, {marchedefruitsetlegumes, 0 }, {mine, 0 }, {restaurant, 0 }, {stade, 0 }, {superette, 0 }, {verger, 0 } };
+        etablissement = new Dictionary<Etablissement, bool>(){ { centrecommercial, false }, { gare, false }, { parcdattraction, false }, { tourradio, false } };
+>>>>>>> Stashed changes
     }
 
     public void spendGold(int i)
@@ -50,5 +55,15 @@ public class Player : MonoBehaviour
     public void earnGold(int i)
     {
         Gold += i;
+    }
+
+    public void AddCard(Cards C)
+    {
+        cards[C] += 1;
+    }
+
+    public void AddEtablissement(Etablissement E)
+    {
+        etablissement[E] = true;
     }
 }
