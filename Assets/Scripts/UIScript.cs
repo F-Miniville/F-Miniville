33330700e671aba603144971cedc5176dde33622
Thibault <108485664+ThibProg@users.noreply.github.com>
2022-10-25 10:16:06 +0200

    Menu Credit Scene1

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        //on rajoute ces quelques lignes pour pouvoir tester le bouton Exit sur Unity
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+                Application.Quit(); //fonctionne seul pour un build
+#endif
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+
+    public void Credit()
+    {
+        SceneManager.LoadScene("Credit");
+    }
+
+    public void Continue()
+    {
+        Debug.Log("Continue ok !"); //waiting for code..
+    }
+
+    public void MenuReturn()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
