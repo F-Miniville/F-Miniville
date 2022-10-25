d18dc696731ba8760e720663416b7dc9091c2ce9
PtiteMadeleine <115978909+PtiteMadeleine@users.noreply.github.com>
2022-10-25 09:25:52 +0200

    player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,10 +8,9 @@ public class Player : MonoBehaviour
     protected int Gold;
     protected string Name;
 
-<<<<<<< Updated upstream
+
     private Champsdeble champsdeble = new Champsdeble();
-<<<<<<< Updated upstream
-=======
+
     private Fabriquedemeuble fabriquedemeuble = new Fabriquedemeuble();
     private Ferme ferme = new Ferme(); 
     private Forêt forêt = new Forêt();
@@ -22,31 +21,13 @@ public class Player : MonoBehaviour
     private Stade stade = new Stade();
     private Superette superette = new Superette();
     private Verger verger = new Verger();
->>>>>>> Stashed changes
 
-    public Player(string name)
-=======
-<<<<<<< HEAD
     public Player(int gold, string name)
->>>>>>> Stashed changes
-    {
-        Gold = 0;
-        Name = name;
-<<<<<<< Updated upstream
-        Cards = new Dictionary<Cards, int> { {champsdeble, 1 }, { new Champsdeble(), 1 } };
-        Cards[champsdeble] += 1;
-=======
-=======
-    private Champsdeble champsdeble = new Champsdeble();
-
-    public Player(string name)
     {
         Gold = 0;
         Name = name;
         Cards = new Dictionary<Cards, int> { {champsdeble, 1 }, { new Champsdeble(), 1 } };
         Cards[champsdeble] += 1;
->>>>>>> main
->>>>>>> Stashed changes
     }
 
     public void spendGold(int i) 
@@ -54,15 +35,7 @@ public class Player : MonoBehaviour
         Gold -= i;
         if (Gold < 0)
         {
-<<<<<<< Updated upstream
             Gold = 0;
-=======
-<<<<<<< HEAD
-            Gold == 0;
-=======
-            Gold = 0;
->>>>>>> main
->>>>>>> Stashed changes
         }
     }
     public void earnGold(int i)
