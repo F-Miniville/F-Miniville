cfb721aae7700786acded3a64f1d7e25c372a1e4
PtiteMadeleine <115978909+PtiteMadeleine@users.noreply.github.com>
2022-10-25 14:33:17 +0200

    again again again

--- Assets/Scripts/Cards/RealCards/Marchedefruitsetlegumes.cs
+++ Assets/Scripts/Cards/RealCards/Marchedefruitsetlegumes.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Marchedefruitsetlegumes : GreenCards
 {
     List<int> activationCost;
-    public override List<int> ActivationCost { get; set; }
+    public new List<int> ActivationCost { get; set; }
 
     int costCards;
     public int CostCards { get; set; }
@@ -13,7 +13,7 @@ public class Marchedefruitsetlegumes : GreenCards
     string cardName;
     public string CardName { get; set; }
 
-    public Marchedefruitsetlegumes()
+    public Marchedefruitsetlegumes(List<int> activationCost, int costCards, string cardName) : base(activationCost, costCards, cardName)
     {
         this.activationCost = new List<int>() { 10, 11 };
         this.costCards = 2;
--- Assets/Scripts/Cards/RealCards/Verger.cs
+++ Assets/Scripts/Cards/RealCards/Verger.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Verger : BlueCards
 {
     List<int> activationCost;
-    public override List<int> ActivationCost { get; set; }
+    public new List<int> ActivationCost { get; set; }
 
     int costCards;
     public int CostCards { get; set; }
@@ -13,7 +13,7 @@ public class Verger : BlueCards
     string cardName;
     public string CardName { get; set; }
 
-    public Verger()
+    public Verger(List<int> activationCost, int costCards, string cardName) : base(activationCost, costCards, cardName)
     {
         this.activationCost = new List<int>() { 10 };
         this.costCards = 3;
