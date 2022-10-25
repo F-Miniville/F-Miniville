0db76aef1e90f240a30c5e5a24e7d9b77a63c4da
rBLOT-Arcenz <r.blot@protonmail.com>
2022-10-25 09:25:38 +0200

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     protected Dictionary<Cards, int> Cards = new Dictionary<Cards, int>();
+    protected Dictionary<Etablissement, bool> Etablissement = new Dictionary<Etablissement, bool>();
     protected int Gold;
     protected string Name;
 
-<<<<<<< Updated upstream
+    private Boulangerie boulangerie = new Boulangerie();
+    private Cafe cafe = new Cafe();
+    private Centredaffaires centredaffaires = new Centredaffaires();
+    private Chainedetele chainedetele = new Chainedetele();
     private Champsdeble champsdeble = new Champsdeble();
-<<<<<<< Updated upstream
-=======
     private Fabriquedemeuble fabriquedemeuble = new Fabriquedemeuble();
-    private Ferme ferme = new Ferme(); 
+    private Ferme ferme = new Ferme();
     private Forêt forêt = new Forêt();
     private Fromagerie fromagerie = new Fromagerie();
     private Marchedefruitsetlegumes marchedefruitsetlegumes = new Marchedefruitsetlegumes();
@@ -22,47 +24,27 @@ public class Player : MonoBehaviour
     private Stade stade = new Stade();
     private Superette superette = new Superette();
     private Verger verger = new Verger();
->>>>>>> Stashed changes
 
-    public Player(string name)
-=======
-<<<<<<< HEAD
-    public Player(int gold, string name)
->>>>>>> Stashed changes
-    {
-        Gold = 0;
-        Name = name;
-<<<<<<< Updated upstream
-        Cards = new Dictionary<Cards, int> { {champsdeble, 1 }, { new Champsdeble(), 1 } };
-        Cards[champsdeble] += 1;
-=======
-=======
-    private Champsdeble champsdeble = new Champsdeble();
+    private Centrecommercial centrecommercial = new Centrecommercial();
+    private Gare gare = new Gare();
+    private Parcdattraction parcdattraction = new Parcdattraction();
+    private Tourradio tourradio = new Tourradio();
 
     public Player(string name)
     {
         Gold = 0;
         Name = name;
-        Cards = new Dictionary<Cards, int> { {champsdeble, 1 }, { new Champsdeble(), 1 } };
+        Cards = new Dictionary<Cards, int>() { { boulangerie, 1 }, { cafe, 0 }, { centredaffaires, 0 }, { chainedetele, 0 }, { champsdeble, 1 }, { fabriquedemeuble, 0 }, { ferme, 0 }, { forêt, 0 }, { fromagerie, 0 }, { marchedefruitsetlegumes, 0 }, { mine, 0 }, { restaurant, 0 }, { stade, 0 }, { superette, 0 }, { verger, 0 } };
+        Etablissement = new Dictionary<Etablissement, bool>() { { centrecommercial, false }, { gare, false }, { parcdattraction, false }, { tourradio, false } };
         Cards[champsdeble] += 1;
->>>>>>> main
->>>>>>> Stashed changes
     }
 
-    public void spendGold(int i) 
+    public void spendGold(int i)
     {
         Gold -= i;
         if (Gold < 0)
         {
-<<<<<<< Updated upstream
             Gold = 0;
-=======
-<<<<<<< HEAD
-            Gold == 0;
-=======
-            Gold = 0;
->>>>>>> main
->>>>>>> Stashed changes
         }
     }
     public void earnGold(int i)
