2d3538afa9607698ccd09c544015e42913bb58fc
PtiteMadeleine <115978909+PtiteMadeleine@users.noreply.github.com>
2022-10-28 16:29:30 +0200

    change audioMenu

--- Assets/Scripts/audioMenu.cs
+++ Assets/Scripts/audioMenu.cs
@@ -10,7 +10,7 @@ public class audioMenu : MonoBehaviour
     {
         if (instance != null)
         {
-            Debug.Log("Il y a plus d'une Instance de DiceManager dans la scene");
+            Debug.Log("Il y a plus d'une Instance de audioMenu dans la scene");
             Destroy(this.gameObject);
             return;
         }
